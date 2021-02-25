510799a9346af049af0be2417dc2ab2fc85f6a32
georgecosmos <76905430+georgecosmos@users.noreply.github.com>
2021-02-25 12:08:24 +0000

    Add files via upload

---
+++ Multiplier/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Multiplier";
+            Console.WindowHeight = 50;
+            Console.ForegroundColor = ConsoleColor.Green;
+            double num01;
+            double num02;
+            Console.Write("Input No1 ");
+            num01 = Convert.ToDouble( Console.ReadLine() );
+            Console.Write("Input a second number ");
+            num02 = Convert.ToDouble(Console.ReadLine() );
+            double answer = num01 * num02;
+            Console.WriteLine("The answer is " + answer);
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+            Console.ReadKey();
+            
+        }
+    }
+}
